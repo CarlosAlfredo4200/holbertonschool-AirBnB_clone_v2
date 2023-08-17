cb4394afc461c9c8826c068b1a84a62ac0a1992f
carlosalfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-08-17 14:47:11 -0500

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    states = storage.all("State").values()
-    return render_template('7-states_list.html', states=states)
+    states = storage.all("State")
+    state_list = list(states.values())
+    return render_template('7-states_list.html', states=state_list)
 
 
 @app.teardown_appcontext
