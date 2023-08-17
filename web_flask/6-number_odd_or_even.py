865edfadc8a4358a8fda1d9ce546174eceb79f60
carlosalfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-08-17 13:59:36 -0500

    6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""Initialization of the Python Flask application"""
+"""Inicialización de la aplicación Python Flask"""
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
-
-'''Route root URL'''
 
 
 @app.route('/', strict_slashes=False)
@@ -13,24 +10,15 @@ def hello_hbnb():
     return "Hello HBNB!"
 
 
-'''Route for /hbnb'''
-
-
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
-
-
-'''Route for /c/<text>'''
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     text = text.replace('_', ' ')
     return f"C {text}"
-
-
-'''Route for /python/<text> with default value "is cool"'''
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
@@ -40,15 +28,9 @@ def python_text(text):
     return f"Python {text}"
 
 
-'''Route for /number/<n>'''
-
-
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_n(n):
     return f"{n} is a number"
-
-
-'''Route for /number_template/<n>'''
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
@@ -58,11 +40,8 @@ def number_template_n(n):
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even_n(n):
-    if n % 2 == 0:
-        parity = 'even'
-    else:
-        parity = 'odd'
-    return render_template('6-number_odd_or_even.html', number=n, type_data=parity)
+    parity = 'even' if n % 2 == 0 else 'odd'
+    return render_template('6-number_odd_or_even.html', number=n, parity=parity)
 
 
 if __name__ == '__main__':
